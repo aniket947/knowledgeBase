22e7830d8a2612c30cc6ced9d69cdcd1aaf56e30
Aniket Mandle <mandle.aniket2@gmail.com>
2022-01-03 09:56:08 +0530

    user info and Qualification changes

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -7,9 +7,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
+
+  // firstName:string | undefined; // declare propery
+
+
+  userObj = {                          // declare property in object
+    userMarriedStatus: '',
+    firstName: '',
+    middleName: '',
+    lastName: '',
+    userAge: '',
+    userGender: '',
+    userDob: '',
+    usermailId: '',
+    userMobile: ''
+  };
+
+  userAddr = {
+    temp: {
+      state: '',
+      city: '',
+      address: '',
+      pincode: ''
+    },
+    perm: {
+      state: '',
+      city: '',
+      address: '',
+      pincode: ''
+    }
+  }
+  qualificationObj = {
+    selectedQualification: '',
+    selectedBoard: '',
+    selectedpercentage: '',
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  copyTempAddress(event: any) {
+    if (event.currentTarget.checked) {
+      this.userAddr.perm = this.userAddr.temp;
+    }
+    else {
+      this.userAddr.perm = {
+        state: '',
+        city: '',
+        address: '',
+        pincode: ''
+      };
+    }
+  }
+
+  saveUserInfo() {
+    console.log(this.userObj);
+  }
+
+  qualification() {
+
+  }
+  addQualificationDetails() {
+
+  }
 }
