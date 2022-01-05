3663e6c45665f5dbda936409d078fc37c573ddde
Aniket Mandle <mandle.aniket2@gmail.com>
2022-01-05 19:02:33 +0530

    Validation Apply on Educational Details

--- src/app/user-details/user-details.component.ts
+++ src/app/user-details/user-details.component.ts
@@ -6,11 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-details.component.css']
 })
 export class UserDetailsComponent implements OnInit {
-
-
-  // firstName:string | undefined; // declare propery
-
-
   userObj = {                          // declare property in object
     userMarriedStatus: '',
     firstName: '',
@@ -22,7 +17,6 @@ export class UserDetailsComponent implements OnInit {
     usermailId: '',
     userMobile: ''
   };
-
   userAddr = {
     temp: {
       state: '',
@@ -36,16 +30,90 @@ export class UserDetailsComponent implements OnInit {
       address: '',
       pincode: ''
     }
-  }
-  qualificationObj = {
-    selectedQualification: '',
-    selectedBoard: '',
-    selectedpercentage: '',
-  }
+  };
+
+  isEditClicked: boolean = false;
+  selectedQualification = '';
+  selectedBoard = '';
+  selectedpercentage = '';
+  isTempPermAddSame: boolean = false;
+  educationDetails: any = []
+  
+  editedPosition: number = -1;
+  userqualification: string | undefined;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addQualification() {
+    let recordFound = this.educationDetails.find((user: any) => {
+      return (user.userqualification===this.selectedQualification )
+    });
+    if(recordFound){
+      alert(this.selectedQualification + ' Qualification already added. Please select another Qualification ');
+      return ;
+    }
+    let validResult = this.validQualificationDetails();
+    if(!validResult){
+      return;
+    }
+    
+    console.log(recordFound);
+
+    if(validResult || recordFound===undefined) {                                    
+      const user = { userqualification: this.selectedQualification, userboard: this.selectedBoard, userpercentage: this.selectedpercentage };///// created objec here
+      this.educationDetails.push(user);
+      this.resetVariables();
+      
+    }
+    
+  }
+
+  updateQualification() {
+    let validResult = this.validQualificationDetails();
+    if (validResult) {
+      const user = { userqualification: this.selectedQualification, userboard: this.selectedBoard, userpercentage: this.selectedpercentage };///// created objec here
+      this.educationDetails[this.editedPosition] = user;
+      this.resetVariables();
+    }
+  }
+
+  deleteUserQuali(selectedPosition: any) {
+    this.educationDetails.splice(selectedPosition, 1);
+  }
+
+  editUserQuali(selectedRecord: any, editedPosition: number) {        //editedposition = edit element position
+    console.log(selectedRecord);
+    this.selectedQualification = selectedRecord.userqualification;
+    this.selectedBoard = selectedRecord.userboard;
+    this.selectedpercentage = selectedRecord.userpercentage;
+    this.isEditClicked = true;
+    this.editedPosition = editedPosition;
+  }
+
+  resetVariables() {
+    this.selectedQualification = '';
+    this.selectedBoard = '';
+    this.selectedpercentage = '';
+    this.isEditClicked = false;
+    this.editedPosition = -1;
+  }
+  validQualificationDetails(){
+    if (this.selectedQualification === '') {
+      alert('Select Qualification');
+      return false;
+    }
+    if (this.selectedBoard === '') {
+      alert('Select Board');
+      return false;
+    }
+    if (this.selectedpercentage === '') {
+      alert('Select Percentage');
+      return false;
+    }
+    return true;
   }
 
   copyTempAddress(event: any) {
@@ -60,16 +128,11 @@ export class UserDetailsComponent implements OnInit {
         pincode: ''
       };
     }
+    this.isTempPermAddSame = event.currentTarget.checked;
   }
 
   saveUserInfo() {
     console.log(this.userObj);
   }
-
-  qualification() {
-
-  }
-  addQualificationDetails() {
-
-  }
 }
+
